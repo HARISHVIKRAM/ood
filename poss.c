e7a53c94ea5bd0b8319a411c95d083f36f276ad3
HARISHVIKRAM <48052662+HARISHVIKRAM@users.noreply.github.com>
2019-03-11 16:32:11 +0530

    Create poss.c

---
+++ poss.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int number;
+ 
+    printf("Enter a number: \n");
+    scanf("%d", &number);
+    if (number > 0)
+        printf("%d is a positive number \n", number);
+    else if (number < 0)
+        printf("%d is a negative number \n", number);
+    else
+        printf("0 is neither positive nor negative");
+}
